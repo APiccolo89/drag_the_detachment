81eff1cf10af4dcc28f4ee1a28c2c4678ff37507
APiccolo89 <85160533+APiccolo89@users.noreply.github.com>
2022-08-02 11:04:51 +0200

    Just a few repairs
    Thinking that is necessary to not dimensionalize the equations as it is difficult to converge to a decent tollerance

--- Compute_dddt_Drag.m
+++ Compute_dddt_Drag.m
@@ -1,4 +1,4 @@
-function Compute_dddt_Drag(D,B_n,B_d,n,L0,D0,drho,etaUM,tau0)
+function Compute_dddt_Drag(D,B_n,B_d,n,L0,D0,drho,etaUM,tau0,name_1)
 %Compute the D,t of the slab detachment. 
 % Input Parameter: D0 = initial length 
 %                  L0 = initial length
@@ -36,15 +36,15 @@ dDdtA = fzero(FUNFzero,dDdt0);
 % Create function handle 
 Funf_wi = @(t,x,xp0) compute_dragODE(x,xp0,B_n,B_d,n,etaUM,drho,D0,L0)
 % Set the option for resolving the system of equation
-options = odeset('RelTol',1e-2,'NormControl','on');
+options = odeset('RelTol',5e-2,'NormControl','on');
 % resolve the system
 [t,D] = ode15i(Funf_wi,[0 20*td],D0, dDdtA,options);
 
 D_norm = D/D0;
 tau_norm = 1./D_norm;
 
-plot_time_evolution(t/td,D_norm,'D',append('D','_',num2str(L0),'_',num2str(D0),'_',num2str(etaUM)),n)
-plot_time_evolution(t/td,tau_norm,'tau',append('tau','_',num2str(L0),'_',num2str(D0),'_',num2str(etaUM)),n)
+plot_time_evolution(t/td,D_norm,'D',append('D',name_1,'_',num2str(L0),'_',num2str(D0),'_',num2str(log10(etaUM))),n)
+plot_time_evolution(t/td,tau_norm,'tau',append('tau',name_1,'_',num2str(L0),'_',num2str(D0),'_',num2str(log10(etaUM))),n)
 
 
 
@@ -96,7 +96,7 @@ if strcmp(name,"D")
     plot(tt.*n,anal,'r')
     scatter(tt(400:400:end).*n,anal(400:400:end),'d')
 end
-xlim([0,4])
+xlim([0,10])
 xlabel('t/td [n.d.]')
 ylabel(append(name, '[n.d.]'))
 print(name_figure,'-dpng')
--- Script_testing.m
+++ Script_testing.m
@@ -7,7 +7,7 @@ clf
 
 eta0 = 1e23; 
 eta0D = 10*eta0;
-L0    = 400e3;
+L0    = 500e3;
 D0    = 100e3; 
 n     = 3.5; 
 tau0  = 100e6;
@@ -15,7 +15,7 @@ drho   = 2*tau0/(9.81*L0);
 B_n   = tau0^(1-n)/eta0;
 B_d   = 1/eta0D; 
 etaUM = 1e22; 
-
+name_1 = num2str(log10(eta0));
 
 D = D0;
-Compute_dddt_Drag(D,B_n,B_d,n,L0,D0,drho,etaUM,tau0); 
+Compute_dddt_Drag(D,B_n,B_d,n,L0,D0,drho,etaUM,tau0,name_1); 
